99010550ab546aa3efceb0466ba3290f35ebd6f1
pritha-tarento <42533384+pritha-tarento@users.noreply.github.com>
2021-09-22 10:53:08 +0530

    imported jsonnode

--- src/main/java/org/sunbird/cb/hubservices/profile/handler/ProfileRequestHandler.java
+++ src/main/java/org/sunbird/cb/hubservices/profile/handler/ProfileRequestHandler.java
@@ -1,6 +1,8 @@
 package org.sunbird.cb.hubservices.profile.handler;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
