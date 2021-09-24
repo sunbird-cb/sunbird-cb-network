ee5237eb8dc6a928c3194252adebefeae2493127
pritha-tarento <42533384+pritha-tarento@users.noreply.github.com>
2021-09-24 15:34:38 +0530

    Update NotificationService.java

--- src/main/java/org/sunbird/cb/hubservices/serviceimpl/NotificationService.java
+++ src/main/java/org/sunbird/cb/hubservices/serviceimpl/NotificationService.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
