6a3d6d8a9f28782aa3a7ee5ee74ec5c8f837dda4
Juhi <juhiagl8@gmail.com>
2023-05-24 12:41:16 +0530

    changes

--- src/main/java/org/sunbird/cb/hubservices/profile/handler/AutoCompleteService.java
+++ src/main/java/org/sunbird/cb/hubservices/profile/handler/AutoCompleteService.java
@@ -31,7 +31,7 @@ public class AutoCompleteService {
 	private RestHighLevelClient esClient;
 
 
-	final String[] includeFields = { "profileDetails.verifiedKarmayogi", "employmentDetails.departmentName", "personalDetails.firstname",
+	final String[] includeFields = { "verifiedKarmayogi", "employmentDetails.departmentName", "personalDetails.firstname",
 			"personalDetails.surname", "personalDetails.primaryEmail", "id", "professionalDetails.name" };
 
 	public List<Map<String, Object>> getUserSearchData(String searchTerm) throws IOException {
